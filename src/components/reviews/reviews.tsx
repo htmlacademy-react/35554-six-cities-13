664e5c277d5ce5fba4858a0c93fa54b005b75dbd
Nanammii <kaddia@gmail.com>
2023-07-29 16:23:14 +0700

    refactoring component reviews

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -1,46 +1,16 @@
 import FormReviews from '../form-reviews/form-reviews';
 import {Comments} from '../../types/comments';
-import * as dayjs from 'dayjs';
+import ReviewsList from '../reviews-list/reviews-list';
 
 type ReviewsProps = {
   comments: Comments;
 };
 
 function Reviews({comments}: ReviewsProps):JSX.Element {
-  const refineDate = (date: string) => date && dayjs(date).format('MMMM YYYY');
-
   return (
     <section className="offer__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{comments.length}</span></h2>
-      <ul className="reviews__list">
-        {comments.map((item) => {
-          const {id, comment, date, rating, user} = item;
-          return (
-            <li className="reviews__item" key={id}>
-              <div className="reviews__user user">
-                <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                  <img className="reviews__avatar user__avatar" src={user.avatarUrl} width="54" height="54"
-                    alt="Reviews avatar"
-                  />
-                </div>
-                <span className="reviews__user-name">{user.name}</span>
-              </div>
-              <div className="reviews__info">
-                <div className="reviews__rating rating">
-                  <div className="reviews__stars rating__stars">
-                    <span style={{width: `${Math.floor(rating) * 20}%`}}></span>
-                    <span className="visually-hidden">Rating</span>
-                  </div>
-                </div>
-                <p className="reviews__text">
-                  {comment}
-                </p>
-                <time className="reviews__time" dateTime="2019-04-24">{refineDate(date)}</time>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      <ReviewsList comments={comments} />
       <FormReviews />
     </section>
   );
