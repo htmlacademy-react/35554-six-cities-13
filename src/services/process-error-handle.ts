f6719f4ea167b8a226bb8126fe0fd412e39a7657
Nanammii <kaddia@gmail.com>
2023-08-11 14:01:22 +0700

    fixed

--- src/services/process-error-handle.ts
+++ src/services/process-error-handle.ts
@@ -5,4 +5,4 @@ import {clearErrorAction} from '../store/api-actions';
 export const processErrorHandle = (message: string): void => {
   store.dispatch(setError(message));
   store.dispatch(clearErrorAction());
-}
+};
